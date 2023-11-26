4e1b54e3b7bbe02f0ce9b37b0cafaae884d594e6
Ardhito Nurhadyansah <ardhitonurhadyansah66@gmail.com>
2023-11-26 23:30:58 +0700

    Add 2FA for admin, with QR Code generator so it could be integrated with Authy, Google Authenticator, or similar apps

--- mfa_nidps/urls.py
+++ mfa_nidps/urls.py
@@ -18,8 +18,21 @@ from django.contrib import admin
 from django.urls import path
 from .views import login_view, otp_view, main_view, logout_view
 
+from django_otp.admin import OTPAdminSite
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django.contrib.auth.models import User
+
+class OTPAdmin(OTPAdminSite):
+    pass
+
+admin_site = OTPAdmin(name='OTPAdmin')
+admin_site.register(User)
+admin_site.register(TOTPDevice)
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
+    path('traditional-admin/', admin.site.urls),
     path('', main_view, name='main'),
     path('login/', login_view, name='login'),
     path('login/otp/', otp_view, name='otp'),
